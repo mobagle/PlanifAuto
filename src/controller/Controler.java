34acdffd2b8d68cf4d894bc9f9ff7cbf69c92830
mobagle <mobagle@gmail.com>
2018-03-24 19:41:23 +0100

    angles corrigés + petite démo

--- src/controller/Controler.java
+++ src/controller/Controler.java
@@ -36,6 +36,11 @@ public class Controler {
 	protected InputHandler input = null;
 	protected Camera camera = null;
 	public static boolean seekLeft;
+	private IntPoint myPos;
+	private IntPoint dest;
+	private double distanceTot;
+	private long runningTimeTot;
+	private long timeForOneUnit;
 
 	enum State {
 		toThePalet, toTheHome
@@ -54,6 +59,10 @@ public class Controler {
 		input = new InputHandler(screen);
 		motors.add(propulsion);
 		motors.add(graber);
+		myPos = new IntPoint(0, 0);
+		distanceTot = 0;
+		runningTimeTot = 0;
+		timeForOneUnit = 2500;
 	}
 
 	/**
@@ -66,6 +75,7 @@ public class Controler {
 	 */
 	public void start() throws IOException, ClassNotFoundException {
 		loadCalibration();
+
 		screen.drawText("Calibration", "Appuyez sur echap ", "pour skipper");
 		boolean skip = input.waitOkEscape(Button.ID_ESCAPE);
 		if (skip || calibration()) {
@@ -82,6 +92,12 @@ public class Controler {
 			}
 			mainLoop();
 		}
+
+		// my code
+		// seekLeft = true;
+		// mainLoop();
+		// end my code
+
 		cleanUp();
 	}
 
@@ -154,17 +170,17 @@ public class Controler {
 	 * Lance pdll pour trouver les actions Ã  effectuer
 	 */
 	private static ArrayList<String> findGoals() {
-		Solver s = new Solver(); 
-		
+		Solver s = new Solver();
+
 		// Rï¿½cuperation des points
 		// camera.get ...
 		// Pour l'instant statics
 		IntPoint marvin = new IntPoint(0, 0);
 		ArrayList<IntPoint> listPalets = new ArrayList<>();
-		for(int i=3; i<10; i=i+3)
-			for(int j=3; j<10; j=j+3)
+		for (int i = 3; i < 10; i = i + 3)
+			for (int j = 3; j < 10; j = j + 3)
 				listPalets.add(new IntPoint(i, j));
-		//listPalets.add(new IntPoint(3, 3));
+		// listPalets.add(new IntPoint(3, 3));
 
 		// Recherche des action a effectuer
 		return s.findActions(marvin, null, listPalets);
@@ -290,63 +306,98 @@ public class Controler {
 		}
 	}
 	
+	private void majTimeToRunByUnit(double dist, long time) {
+		System.out.println("Old TimeToRunByUnit: " + timeForOneUnit +" ms | dist: "+ dist+" | time: "+time);
+		distanceTot += dist;
+		runningTimeTot +=time;
+		timeForOneUnit = (long) (runningTimeTot/distanceTot);
+		System.out.println("New TimeToRunByUnit: " + timeForOneUnit +" ms");
+	}
+
 	private boolean execute(String ac) {
 		// X largeur du terrain, Y longueur
+		screen.drawText("Action", ac);
 		System.out.println(ac);
 		String action[] = ac.split(" ");
-		int x0, x1, y0, y1, distance;
+		int x0, x1, y0, y1;
 		for (TImedMotor m : motors)
 			m.checkState();
 		switch (action[0]) {
 		case "prendrePalet":
+			boolean ret = false;
+			double distance = distance(myPos, dest);
+			int ttl = (int)(timeForOneUnit * distance);
 			// avance jusqu'ï¿½ toucher le palet
-			propulsion.runFor(R2D2Constants.MAX_GRABING_TIME, true);
-			while (vision.getRaw()[0] > R2D2Constants.COLLISION_DISTANCE && !pression.isPressed()) {
-				// si pas trouvï¿½ de palet aprï¿½s un certain temps, sort
-				if (!propulsion.isRunning() || input.escapePressed())
-					return false;
+			propulsion.runFor(ttl,true);
+			long start = System.currentTimeMillis();
+			long elapsedTime = 0;
+			while (propulsion.isRunning()) {
 				propulsion.checkState();
+				/*
+				 * if(vision.getRaw()[0] <= R2D2Constants.COLLISION_DISTANCE) { // arret des
+				 * roues propulsion.stopMoving(); System.out.println("COLLISION"); return false;
+				 * } else
+				 */
+				if (pression.isPressed()) {
+					// arret des roues
+					System.out.println("PRESSION");
+					propulsion.stopMoving();
+					elapsedTime = System.currentTimeMillis() - start;
+					majTimeToRunByUnit(distance, elapsedTime);
+					ret = true;
+					break;
+				}
 			}
-			// arret des roues
-			propulsion.stopMoving();
-			// ferme les pince
 			graber.close();
 			// attend la fin de la fermeture des pinces
 			while (graber.isRunning()) {
 				graber.checkState();
-				if (input.escapePressed())
-					return false;
 			}
-			return true;
+			return ret;
 
 		case "lacherPalet":
+			distance = distance(myPos, dest);
+			ttl = (int)(timeForOneUnit * distance);
+			// avance jusqu'ï¿½ toucher le palet
+			propulsion.runFor(ttl,true);
+			while (propulsion.isRunning()) {
+				propulsion.checkState();
+			}
 			// ouvre les pinces
 			graber.open();
 			// attend la fin de l'ouverture des pinces
-			while (graber.isRunning())
-				;
+			while (graber.isRunning()) {
+				graber.checkState();
+			}
+
 			// recule aprï¿½s avoir lacher le palet
 			propulsion.runFor(R2D2Constants.EMPTY_HANDED_STEP_FORWARD, false);
 			while (propulsion.isRunning()) {
 				propulsion.checkState();
-				if (input.escapePressed())
-					return false;
 			}
 			return true;
 
-		case "deplacement": // vers un x plus grand
+		case "deplacement":
+			//initialisation des points
 			x0 = Integer.valueOf(action[1]);
 			x1 = Integer.valueOf(action[2]);
 			y0 = Integer.valueOf(action[3]);
 			y1 = Integer.valueOf(action[4]);
-			
+			myPos = new IntPoint(x0, y0);
+			dest = new IntPoint(x1, y1);
+			IntPoint north = new IntPoint(x0, 12);
 			// rotation
-			double myOrientation = propulsion.getOrientation();
-			double angleFromNorth = Math.atan2(y1 - y0, x1 - x0) - Math.atan2(12 - y0, x0 - x0);
-			Double rotation =  angleFromNorth - myOrientation;
-			System.out.println("myOrientation: "+myOrientation+" | angleFromNorth: "+angleFromNorth+" | rotation: "+rotation);
+
+			int degrees = angle(myPos, dest, north);
+			System.out.println("angle calcul: " + degrees);
 			
-			propulsion.rotate(rotation.intValue(), true, R2D2Constants.ANGLE_CORRECTION);
+			int myOrientation = (int)propulsion.getRotateToNorth();
+			System.out.println("mon orientation: " + myOrientation);
+			
+			int angle = degrees - myOrientation;
+			System.out.println("angle corrected: " + angle);
+
+			propulsion.rotate(Math.abs(angle), false, true);
 			while (propulsion.isRunning()) {
 				propulsion.checkState();
 			}
@@ -358,28 +409,53 @@ public class Controler {
 		}
 	}
 
+	/** retourne la distance absolue entreles points p1 et p2 */
+	private double distance(IntPoint p1, IntPoint p2) {
+		return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
+	}
+
+	/** retourne l'angle en degrees en p1 formé par les points p2 et p3*/
+	private int angle(IntPoint p1, IntPoint p2, IntPoint p3) {
+		double ab = distance(p1, p2);
+		double ac = distance(p1, p3);
+		double bc = distance(p3, p2);
+		double rad = Math.acos(((Math.pow(ab, 2) + Math.pow(ac, 2) - Math.pow(bc, 2)) / (2 * ab * ac)));
+		Double degree = (rad * 180) / Math.PI;
+		return degree.intValue();
+	}
 
 	private void mainLoop() {
-		
+
 		boolean run = true;
 		boolean pasDeProbleme = true;
 		ArrayList<String> goals = new ArrayList<>();
 		String goal;
-		while(run){
-			goals = findGoals();
-			if(goals != null){
-				ListIterator<String> li = goals.listIterator();
-				while (li.hasNext() && pasDeProbleme) {
-					goal = li.next();
-					if(goal != null){
-						pasDeProbleme = execute(goal);
-					}
-				}
-			} else {
-				run = false;
-			}	
+
+		goals.add("deplacement 0 2 0 3");
+		goals.add("prendrePalet");
+		goals.add("deplacement 2 0 3 0");
+		goals.add("lacherPalet");
+		goals.add("deplacement 0 0 0 3");
+		goals.add("prendrePalet");
+		goals.add("deplacement 0 0 3 0");
+		goals.add("lacherPalet");
+
+		ListIterator<String> li = goals.listIterator();
+		while (li.hasNext() && pasDeProbleme) {
+			goal = li.next();
+			if (goal != null) {
+				pasDeProbleme = execute(goal);
+			}
 		}
-		
+
+		/**
+		 * real loop after while(run){ goals = findGoals(); if(goals != null){
+		 * ListIterator<String> li = goals.listIterator(); while (li.hasNext() &&
+		 * pasDeProbleme) { goal = li.next(); if(goal != null){ pasDeProbleme =
+		 * execute(goal); } } } else { run = false; }
+		 * 
+		 * }
+		 */
 	}
 
 	/**
